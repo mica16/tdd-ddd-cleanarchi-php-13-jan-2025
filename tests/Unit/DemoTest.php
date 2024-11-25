2a1d9b50abaef6a83e90e570fa104f46b41cea54
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2024-11-25 02:09:16 +0100

    init project with phpunit and infection

---
+++ tests/Unit/DemoTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Demo;
+use Monolog\Test\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertEquals('Hello World', (new Demo())->returnSomething());
+    }
+}
